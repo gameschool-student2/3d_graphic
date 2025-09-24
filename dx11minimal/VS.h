9dcc4aa2f64a17b6417c494966877ad011ee4092
gs <stud2.gsch124@outlook.com>
2025-09-24 18:02:14 +0700

    wip

--- dx11minimal/VS.h
+++ dx11minimal/VS.h
@@ -76,9 +76,9 @@ VS_OUTPUT VS(uint vID : SV_VertexID)
     float4 pos = float4(p, 0, 1);
     pos.y += col * 2;
     pos.x += row * 2;
+    pos.xy -= (float)n - 1;
 
     pos.xyz = cylinder(pos.xy);
-    pos.y -= (float)n - 1;
 
     output.pos = mul(pos, mul(view[0], proj[0]));
     output.uv = float2(1, -1) * p / 2. + .5;
