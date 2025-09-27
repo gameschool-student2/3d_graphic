3e6dc205dbdf84a201addff1888fdccb55129d6e
gs <stud2.gsch124@outlook.com>
2025-09-27 15:05:31 +0700

    wip

--- dx11minimal/VS.h
+++ dx11minimal/VS.h
@@ -83,15 +83,15 @@ VS_OUTPUT VS(uint vID : SV_VertexID)
     float3 pos1 = ball(pos.xy + float2(1, 0));
     float3 pos2 = ball(pos.xy + float2(0, 1));
 
-    float3 t = normalize(pos1 - pos0);
-    float3 b = normalize(pos2 - pos0);
-    float3 h = normalize(cross(t, b));
+    float3 tangent = normalize(pos1 - pos0);
+    float3 binormal = normalize(pos2 - pos0);
+    float3 normal = cross(tangent, binormal);
 
     output.pos = mul(float4(pos0.xyz, 1), mul(view[0], proj[0]));
     output.vpos = mul(output.pos, view[0]);
     output.wpos = float4(pos0.xyz, 1);
     output.uv = pos.xy / n;
-    output.vnorm = float4(h.xyz, 1);
+    output.vnorm = float4(normal.xyz, 1);
 
     return output;
 }
