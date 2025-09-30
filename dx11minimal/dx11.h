1dc4f69687b5333c10ef6fd69c2248b6b8234be2
gs <stud2.gsch124@outlook.com>
2025-09-30 17:38:18 +0700

    wip

--- dx11minimal/dx11.h
+++ dx11minimal/dx11.h
@@ -940,7 +940,7 @@ void mainLoop()
 	Blend::Blending(Blend::blendmode::alpha, Blend::blendop::add);
 
 	Textures::RenderTarget(0, 0);
-	Draw::Clear({ 0, 0, 1, 0 });
+	Draw::Clear({ 0.15, 0.15, 0.15, 0 });
 	Draw::ClearDepth();
 	Depth::Depth(Depth::depthmode::on);
 	Rasterizer::Cull(Rasterizer::cullmode::wireframe);
