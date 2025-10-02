4ce7e6f9e758aed93b8606dddde499aa2c442693
gs <stud2.gsch124@outlook.com>
2025-10-02 12:41:07 +0700

    wip

--- dx11minimal/dx11.h
+++ dx11minimal/dx11.h
@@ -10,6 +10,7 @@
 #include "DirectXMath.h"
 #include <DirectXPackedVector.h>
 #include <debugapi.h>
+#include <stdio.h>
 
 using namespace DirectX;
 
@@ -160,6 +161,15 @@ namespace Textures
 	} textureDesc;
 
 	textureDesc Texture[max_tex];
+
+	struct TargaHeader
+	{
+		unsigned char data1[12];
+		unsigned short width;
+		unsigned short height;
+		unsigned char bpp;
+		unsigned char data2;
+	};
 
 	byte currentRT = 0;
 
@@ -379,6 +389,95 @@ namespace Textures
 		SetViewport(target, level);
 	}
 
+
+	bool LoadTarga32Bit(char* filename)
+	{
+		int error, bpp, imageSize, index, i, j, k;
+		FILE* filePtr;
+		unsigned int count;
+		TargaHeader targaFileHeader;
+		unsigned char* targaImage;
+
+
+		// Open the targa file for reading in binary.
+		error = fopen_s(&filePtr, filename, "rb");
+		if (error != 0)
+		{
+			return false;
+		}
+
+		// Read in the file header.
+		count = (unsigned int)fread(&targaFileHeader, sizeof(TargaHeader), 1, filePtr);
+		if (count != 1)
+		{
+			return false;
+		}
+
+		// Get the important information from the header.
+		m_height = (int)targaFileHeader.height;
+		m_width = (int)targaFileHeader.width;
+		bpp = (int)targaFileHeader.bpp;
+
+		// Check that it is 32 bit and not 24 bit.
+		if (bpp != 32)
+		{
+			return false;
+		}
+
+		// Calculate the size of the 32 bit image data.
+		imageSize = m_width * m_height * 4;
+
+		// Allocate memory for the targa image data.
+		targaImage = new unsigned char[imageSize];
+
+		// Read in the targa image data.
+		count = (unsigned int)fread(targaImage, 1, imageSize, filePtr);
+		if (count != imageSize)
+		{
+			return false;
+		}
+
+		// Close the file.
+		error = fclose(filePtr);
+		if (error != 0)
+		{
+			return false;
+		}
+
+		// Allocate memory for the targa destination data.
+		m_targaData = new unsigned char[imageSize];
+
+		// Initialize the index into the targa destination data array.
+		index = 0;
+
+		// Initialize the index into the targa image data.
+		k = (m_width * m_height * 4) - (m_width * 4);
+
+		// Now copy the targa image data into the targa destination array in the correct order since the targa format is stored upside down and also is not in RGBA order.
+		for (j = 0; j < m_height; j++)
+		{
+			for (i = 0; i < m_width; i++)
+			{
+				m_targaData[index + 0] = targaImage[k + 2];  // Red.
+				m_targaData[index + 1] = targaImage[k + 1];  // Green.
+				m_targaData[index + 2] = targaImage[k + 0];  // Blue
+				m_targaData[index + 3] = targaImage[k + 3];  // Alpha
+
+				// Increment the indexes into the targa data.
+				k += 4;
+				index += 4;
+			}
+
+			// Set the targa image data index back to the preceding row at the beginning of the column since its reading it in upside down.
+			k -= (m_width * 8);
+		}
+
+		// Release the targa image data now that it was copied into the destination array.
+		delete[] targaImage;
+		targaImage = 0;
+
+		return true;
+	}
 }
 
 
@@ -856,6 +955,8 @@ void Dx11Init()
 	
 	//main RT
 	Textures::Create(0, Textures::tType::flat, Textures::tFormat::u8, XMFLOAT2(width, height), false, true);
+
+	Textures::Create(1, Textures::tType::flat, Textures::tFormat::u8, XMFLOAT2(width, height), false, true);
 }
 
 
