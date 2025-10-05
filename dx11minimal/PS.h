47ac456516026d0a31596bc12cce5d062234abea
GS <GS@DESKTOP-5PH2BTB>
2025-10-05 14:47:07 +0700

    wip

--- dx11minimal/PS.h
+++ dx11minimal/PS.h
@@ -49,7 +49,7 @@ float4 PS(VS_OUTPUT input) : SV_Target
     projCoords.y = 1.0 - projCoords.y; // Инвертируем Y
 
     // Сравниваем глубину фрагмента с глубиной из карты теней
-    float shadow = shadowMap.SampleCmpLevelZero(shadowSampler, projCoords.xy, projCoords.z).r;
+    float shadow = shadowMap.SampleCmpLevelZero(shadowSampler, projCoords.xy, projCoords.z);
 
     float3 lightDir = view[1]._m02_m12_m22;
     float4 ambientColor = float4(0.15, 0.15, 0.15, 1);
